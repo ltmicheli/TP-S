6a500b8093d1a3a1d1a24934cb4f857991a3c7f1
ltmicheli <lucasmicheli@live.com.ar>
2015-04-17 00:10:26 -0300

    tp 0 cpp
    tp 0 cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * TP 0#
+ * File:   main.cpp
+ * Author: Lucas
+ * Hola Mundo!
+ * Created on 31 de marzo de 2015, 13:32
+ */
+
+#include <iostream>
+
+
+/*
+ * 
+ */
+int main() 
+{
+    std :: cout << "Hola Mundo!";
+}
+
